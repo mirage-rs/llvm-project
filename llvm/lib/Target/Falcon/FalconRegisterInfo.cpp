52db53ab76aed796a9e07dfd3cf5cf24ce1d3b45
Valentin B <valentin.be@protonmail.com>
2020-12-01 17:02:29 +0100

    Start working on Falcon register definitions

---
+++ llvm/lib/Target/Falcon/FalconRegisterInfo.cpp
@@ -0,0 +1,12 @@
+//===-- FalconRegisterInfo.cpp - Falcon Register Information --------------===//
+//
+// Part of the LLVM fork for the Mirage project, under the Apache License v2.0
+// with LLVM Exceptions. See https://llvm.org/LICENSE.txt for license
+// information.
+// SPDX-License-Identifier: Apache-2.0 with LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// This file contains the Falcon implementation of the TargetRegisterInfo class.
+//
+//===----------------------------------------------------------------------===//
---
+++ llvm/lib/Target/Falcon/FalconRegisterInfo.h
@@ -0,0 +1,12 @@
+//===-- FalconRegisterInfo.h - Falcon Register Information Impl -*- C++ -*-===//
+//
+// Part of the LLVM fork for the Mirage project, under the Apache License v2.0
+// with LLVM Exceptions. See https://llvm.org/LICENSE.txt for license
+// information.
+// SPDX-License-Identifier: Apache-2.0 with LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// This file contains the Falcon implementation of the TargetRegisterInfo class.
+//
+//===----------------------------------------------------------------------===//
