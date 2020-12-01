9240782000e68ec919e8715da0f0902359a8ce6c
Valentin B <valentin.be@protonmail.com>
2020-12-01 01:01:48 +0100

    Add Falcon subtarget impl and HasSCP feature

---
+++ llvm/lib/Target/Falcon/FalconSubtarget.cpp
@@ -0,0 +1,51 @@
+//===-- FalconSubtarget.cpp - Falcon Subtarget Information ----------------===//
+//
+// Part of the LLVM fork for the Mirage project, under the Apache License v2.0
+// with LLVM Exceptions. See https://llvm.org/LICENSE.txt for license
+// information.
+// SPDX-License-Identifier: Apache-2.0 with LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// This file implements the Falcon-specific subclass of TargetSubtargetInfo.
+//
+//===----------------------------------------------------------------------===//
+
+#include "FalconSubtarget.h"
+
+#include "llvm/Support/TargetRegistry.h"
+
+#include "Falcon.h"
+#include "FalconTargetMachine.h"
+#include "MCTargetDesc/FalconMCTargetDesc.h"
+
+#define DEBUG_TYPE "falcon-subtarget"
+
+#define GET_SUBTARGETINFO_TARGET_DESC
+#define GET_SUBTARGETINFO_CTOR
+#include "FalconGenSubtargetInfo.inc"
+
+namespace llvm {
+
+FalconSubtarget::FalconSubtarget(const Triple &TT, const std::string &CPU,
+                                 const std::string &FS, const TargetMachine &TM)
+    : FalconGenSubtargetInfo(TT, CPU, FS),
+
+      // Subtarget features
+      HasSCP(false),
+
+      InstrInfo(), FrameLowering(),
+      TLInfo(TM, initializeSubtargetDependencies(CPU, FS, TM)) TSInfo() {
+  // Parse features string.
+  ParseSubtargetFeatures(CPU, FS);
+}
+
+FalconSubtarget &
+FalconSubtarget::initializeSubtargetDependencies(StringRef CPU, StringRef FS,
+                                                 const TargetMachine &TM) {
+  // Parse features string.
+  ParseSubtargetFeatures(CPU, FS);
+  return *this;
+}
+
+} // end namespace llvm
